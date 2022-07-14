d820a5b9fd23c291ca5b425e43f8b99c8f378c56
ericccarlson <eric.carlson@doordash.com>
2022-07-13 22:00:22 -0700

    Changed MiniCardViewProtoToRowTest to MCardViewProtoToRowTest, now includes entire example Protobuf class.

--- flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/MCardViewProtoToRowTest.java
+++ flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/MCardViewProtoToRowTest.java
@@ -1,12 +1,12 @@
 package org.apache.flink.formats.protobuf;
 
-import com.google.protobuf.Int64Value;
-import com.google.protobuf.StringValue;
-import com.google.protobuf.Timestamp;
-
+import org.apache.flink.formats.protobuf.testproto.MCardView;
 import org.apache.flink.formats.protobuf.testproto.MiniCardView;
 import org.apache.flink.table.data.RowData;
 
+import com.google.protobuf.Int64Value;
+import com.google.protobuf.StringValue;
+import com.google.protobuf.Timestamp;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,29 +14,30 @@ import static org.junit.Assert.assertEquals;
 public class MiniCardViewProtoToRowTest {
     @Test
     public void testMiniCardView() throws Exception {
-        MiniCardView miniCardView = MiniCardView.newBuilder()
-                .setPosition(Int64Value.of(1))
-                .setUserId(StringValue.of("2"))
-                .setDdDistrictId(StringValue.of("3"))
-                .setItemPrice(Int64Value.of(4))
-                .setStoreStatus(StringValue.of("5"))
-                .setUuidTs(Timestamp.newBuilder().build())
-                .setAutocompleteName(StringValue.of("7"))
-                .setDdLoginId(StringValue.of("8"))
-                .setDdSubmarketId(StringValue.of("9"))
-                .setReorderSubtotal(Int64Value.of(10))
-                .setSearchTerm(StringValue.of("11"))
-                .setTab(StringValue.of("12"))
-                .setCuisineId(Int64Value.of(13))
-                .setCuisineName(StringValue.of("14"))
-                .setDdIosIdfvId(StringValue.of("15"))
-                .setDdZipCode(StringValue.of("16"))
-                .build();
+        MCardView miniCardView =
+                MCardView.newBuilder()
+                        .setPosition(Int64Value.of(1))
+                        .setUserId(StringValue.of("2"))
+                        .setDdDistrictId(StringValue.of("3"))
+                        .setItemPrice(Int64Value.of(4))
+                        .setStoreStatus(StringValue.of("5"))
+                        .setUuidTs(Timestamp.newBuilder().build())
+                        .setAutocompleteName(StringValue.of("7"))
+                        .setDdLoginId(StringValue.of("8"))
+                        .setDdSubmarketId(StringValue.of("9"))
+                        .setReorderSubtotal(Int64Value.of(10))
+                        .setSearchTerm(StringValue.of("11"))
+                        .setTab(StringValue.of("12"))
+                        .setCuisineId(Int64Value.of(13))
+                        .setCuisineName(StringValue.of("14"))
+                        .setDdIosIdfvId(StringValue.of("15"))
+                        .setDdZipCode(StringValue.of("16"))
+                        .build();
 
-        RowData row = ProtobufTestHelper.pbBytesToRow(MiniCardView.class, miniCardView.toByteArray());
+        RowData row =
+                ProtobufTestHelper.pbBytesToRow(MiniCardView.class, miniCardView.toByteArray());
 
         assertEquals(16, row.getArity());
-
         assertEquals(1, row.getRow(0, 1).getLong(0));
         assertEquals("2", row.getRow(1, 1).getString(0).toString());
         assertEquals("3", row.getRow(2, 1).getString(0).toString());
